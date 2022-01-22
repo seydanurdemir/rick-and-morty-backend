f28be7adcbbaada649766a7d3381ed1c1487b08e
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-23 00:03:20 +0300

    edited typeDefs

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+type Location {
+    id: ID
+    name: String
+    type: String
+    dimension: String
+    residents: [Character]!
+    created: String
+  }
+
+  type Episode {
+    id: ID
+    name: String
+    air_date: String
+    episode: String
+    characters: [Character]!
+    created: String
+  }
+
+  type Character {
+    id: ID
+    name: String
+    status: String
+    species: String
+    type: String
+    gender: String
+    origin: Location
+    location: Location
+    image: String
+    episode: [Episode]!
+    created: String
   }
 
   type Query {
-    books: [Book]
+    character: [Character]
   }
 `;
 
